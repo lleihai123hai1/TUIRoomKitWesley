a6d0b19869832b036f3426e4586f19bf189d0b6e
wesleylei <876732861@qq.com>
2023-05-31 17:03:08 +0800

    本地录制开启

--- iOS/TUIRoomKit/Source/LocalRecord/KF/KFAudioConfig.h
+++ iOS/TUIRoomKit/Source/LocalRecord/KF/KFAudioConfig.h
@@ -12,8 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 @interface KFAudioConfig : NSObject
 + (instancetype)defaultConfig;
 
-@property (nonatomic, assign) NSUInteger channels; // 声道数，default: 2。
-@property (nonatomic, assign) NSUInteger sampleRate; // 采样率，default: 44100。
+@property (nonatomic, assign) NSUInteger channels; // 声道数，default: 1。
+@property (nonatomic, assign) NSUInteger sampleRate; // 采样率，default: 48000。
 @property (nonatomic, assign) NSUInteger bitDepth; // 量化位深，default: 16。
 @end
 
