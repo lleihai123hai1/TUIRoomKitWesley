e6de125307df2d59b1beeca93d082030708156a4
wesleylei <876732861@qq.com>
2023-06-07 12:15:34 +0800

    调整为双声道，采集为music类型

--- iOS/TUIRoomKit/Source/LocalRecord/KF/KFAudioConfig.h
+++ iOS/TUIRoomKit/Source/LocalRecord/KF/KFAudioConfig.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface KFAudioConfig : NSObject
 + (instancetype)defaultConfig;
 
-@property (nonatomic, assign) NSUInteger channels; // 声道数，default: 1。
+@property (nonatomic, assign) NSUInteger channels; // 声道数，default: 2。
 @property (nonatomic, assign) NSUInteger sampleRate; // 采样率，default: 48000。
 @property (nonatomic, assign) NSUInteger bitDepth; // 量化位深，default: 16。
 @end
